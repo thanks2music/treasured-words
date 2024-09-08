7f63abc478d203cf2fa090a57ea27652801d39df
Yoshiyuki Ito <thanks2music@gmail.com>
2024-09-08 19:51:08 +0900

    Add CSS animation text

--- with-eslint-app/pages/index.js
+++ with-eslint-app/pages/index.js
@@ -1,8 +1,40 @@
+import 'modern-css-reset/dist/reset.min.css'
+import styles from "../home.module.sass";
+
+const curry = f => (...args) =>
+  args.length >= f.length
+    ? f(...args)
+    : curry(f.bind(f, ...args))
+
+const compose = (f, g) => x => f(g(x))
+const composeN = (...fns) => (...args) =>
+  fns.reverse()
+    .reduce((x, f) => f.apply(f, [].concat(x)), args)
+
+
 const Home = () => (
-  <div>
-    <script src="https://fake-script.com" />
-    <p>Home</p>
-  </div>
+  <>
+    <div className={styles.stage}>
+      <div className={styles.wrapper}>
+        <div className={styles.slash}></div>
+        <div className={styles.sides}>
+          <div className={styles.side}></div>
+          <div className={styles.side}></div>
+          <div className={styles.side}></div>
+          <div className={styles.side}></div>
+        </div>
+        <div className={styles.text}>
+          <div className={styles.textBacking}>WE ARE ALL ONE</div>
+          <div className={styles.textLeft}>
+            <div className={styles.inner}>WE ARE ALL ONE</div>
+          </div>
+          <div className={styles.textRight}>
+            <div className={styles.inner}>WE ARE ALL ONE</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
 );
 
 export default Home;
