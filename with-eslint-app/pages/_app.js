de10cfc58a89ba0cd6a4abd43d14b31fda06d4c2
Yoshiyuki Ito <thanks2music@gmail.com>
2024-09-08 21:43:51 +0900

    Add static export setting

---
+++ with-eslint-app/pages/_app.js
@@ -0,0 +1,5 @@
+import 'modern-css-reset/dist/reset.min.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
--- with-eslint-app/pages/index.js
+++ with-eslint-app/pages/index.js
@@ -1,16 +1,4 @@
-import 'modern-css-reset/dist/reset.min.css'
 import styles from "../home.module.sass";
-
-const curry = f => (...args) =>
-  args.length >= f.length
-    ? f(...args)
-    : curry(f.bind(f, ...args))
-
-const compose = (f, g) => x => f(g(x))
-const composeN = (...fns) => (...args) =>
-  fns.reverse()
-    .reduce((x, f) => f.apply(f, [].concat(x)), args)
-
 
 const Home = () => (
   <>
